07dfcd25424247c139b16d92dad08e1ac23e9422
unknown <Nursahid.arya21@gmail.com>
2021-12-15 04:27:58 +0800

    repair upload and edit

--- public/js/script.js
+++ public/js/script.js
@@ -19,6 +19,7 @@ $(function(){
     $('.add_employee').on('click', function(){
         $('#formModalLabel').html('Tambah data Karyawan');
         $(".modal-footer button[type=submit]").html('Tambah data');
+        $('.modal-body form' ).attr('action', 'http://localhost/Attendance-App/public/Employee/add');
         $('#name').val('');
         $('#unique').val('');
         $('#password').val('');
@@ -35,7 +36,7 @@ $(function(){
         const id = $(this).data('id');
         console.log(id);
         $.ajax({
-            url: "http://localhost:8080/Attendance-App/public/Employee/getEmployee",
+            url: "http://localhost/Attendance-App/public/Employee/getEmployee",
             data: {id : id},
             method: 'post',
             dataType: 'json',
@@ -44,7 +45,7 @@ $(function(){
                 $('#unique').val(data.employee_unique_number);
                 $('#password').val(data.employee_password);
                 $('#lab').text(data.employee_image);
-                $('#old_image').text(data.employee_image);
+                $('#old_image').val(data.employee_image);
                 $('#show').attr('src', 'http://localhost/Attendance-App/public/img/profile_employee/' + data.employee_image);
                 $('#id').val(data.employee_id);
                 console.log(data);
