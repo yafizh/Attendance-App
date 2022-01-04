fb3957226f27eba3508e5bd982ff66262f9b2bba
yafizh <a.chairil.yafizh@gmail.com>
2022-01-05 05:54:09 +0800

    make change password input field responsive

--- app/views/account/index.php
+++ app/views/account/index.php
@@ -8,27 +8,35 @@
     <div class="row" style="margin-left:7px">
         <?php Flasher::flash(); ?>
     </div>
-    <div class="d-flex">
-        <form action="<?= BASEURL; ?>/Account/changePassword" method="POST" class="w-25">
-            <div class="right-inner-addon input-container form-group">
+    <form action="<?= BASEURL; ?>/Account/changePassword" method="POST" class="w-100">
+        <div class="row">
+            <div class="right-inner-addon input-container form-group col-md-6 col-sm-12 col-xl-4">
                 <input type="password" class="form-control" id="old-password" name="old-password" placeholder="Password Lama" autocomplete="off">
             </div>
-
-            <div class="right-inner-addon input-container form-group">
+        </div>
+        <div class="row">
+            <div class="right-inner-addon input-container form-group col-md-6 col-sm-12 col-xl-4">
                 <input type="password" id="new-password" name="new-password" class="form-control" placeholder="Password Baru" autocomplete="off">
             </div>
-
-            <div class="right-inner-addon input-container form-group">
+        </div>
+        <div class="row">
+            <div class="right-inner-addon input-container form-group col-md-6 col-sm-12 col-xl-4">
                 <input type="password" id="confirm-new-password" name="confirm-new-password" class="form-control " placeholder="Konfirmasi Password Baru" autocomplete="off">
             </div>
-            <div class="form-check form-check-inline pl-1">
+        </div>
+        <div class="row">
+            <div class="form-check form-check-inline pl-1 ml-3">
                 <input class="form-check-input" id="toggle-password" type="checkbox" onclick="togglePassword()">
                 <label style="text-indent: 5px;" class="form-check-label" for="toggle-password">Show Password</label>
             </div>
-            <br><br>
-            <button type="submit" class="btn btn-primary btn-block">Ganti Password</button>
-        </form>
-    </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xl-4">
+                <button type="submit" class="btn btn-primary btn-block">Ganti Password</button>
+            </div>
+        </div>
+    </form>
 </div>
 
 <!-- End of Main Content -->
